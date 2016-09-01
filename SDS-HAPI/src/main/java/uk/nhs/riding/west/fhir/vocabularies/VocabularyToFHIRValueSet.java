4f6c1535baf525c114797ddcf5ec27a0cbb6741d
mayfielk <mayfielk@LTH1590700.trust.leedsth.nhs.uk>
2016-09-01 14:46:35 +0100

    Moved non Snomed valuesets to codesystems

--- SDS-HAPI/src/main/java/uk/nhs/riding/west/fhir/vocabularies/VocabularyToFHIRValueSet.java
+++ SDS-HAPI/src/main/java/uk/nhs/riding/west/fhir/vocabularies/VocabularyToFHIRValueSet.java
@@ -7,18 +7,19 @@ import javax.xml.bind.Unmarshaller;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.hl7.fhir.dstu3.model.CodeSystem;
+import org.hl7.fhir.dstu3.model.CodeSystem.ConceptDefinitionComponent;
 import org.hl7.fhir.dstu3.model.Enumerations.ConformanceResourceStatus;
 import org.hl7.fhir.dstu3.model.Extension;
+import org.hl7.fhir.dstu3.model.Identifier;
 import org.hl7.fhir.dstu3.model.StringType;
 import org.hl7.fhir.dstu3.model.UriType;
 import org.hl7.fhir.dstu3.model.ValueSet;
 import org.hl7.fhir.dstu3.model.ValueSet.ConceptReferenceComponent;
 import org.hl7.fhir.dstu3.model.ValueSet.ConceptSetComponent;
 import org.hl7.fhir.dstu3.model.ValueSet.ValueSetComposeComponent;
-
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.model.primitive.StringDt;
-import ca.uhn.fhir.model.primitive.UriDt;
+
 
 public class VocabularyToFHIRValueSet implements Processor {
 
@@ -37,21 +38,11 @@ public class VocabularyToFHIRValueSet implements Processor {
         
 		Vocabulary vocab = (Vocabulary) unmarshaller.unmarshal(xmlContentBytes);
 		
-		ValueSet valueSet = new ValueSet();
-		ConceptSetComponent concepts = new ConceptSetComponent();
 		
-		Extension extension = new Extension();
-		extension.setUrl("http://hl7.org/fhir/StructureDefinition/valueset-oid");
-		extension.setValue(new UriType("urn:oid:"+vocab.getId()));
-		valueSet.addExtension(extension);
-		
-		extension = new Extension();
-		extension.setUrl("http://west.riding.nhs.uk/fhir/StructureDefinition/vocabulary-filename");
-		extension.setValue(new StringType(exchange.getIn().getHeader(Exchange.FILE_NAME).toString()));
-		valueSet.addExtension(extension);
 		
 		// Stop http FHIR queries from processing input parameters
 		//exchange.getIn().removeHeaders("*");
+		
 		String vocabName = vocab.getName();
 		exchange.getIn().setHeader("ActiveStatus", vocab.getStatus());
 		switch(vocab.getStatus())
@@ -59,21 +50,15 @@ public class VocabularyToFHIRValueSet implements Processor {
 			case "active" :
 			case "Active" :
 			case "created" :
-				valueSet.setStatus(ConformanceResourceStatus.ACTIVE);
 				exchange.getIn().setHeader("ActiveStatus", "active" );
 				break;
-			case "superseded" :
-				valueSet.setStatus(ConformanceResourceStatus.RETIRED);
-				break;
 			default:
-				valueSet.setStatus(ConformanceResourceStatus.NULL);
 		}
-		
+		String system = new String();
 		
 		if (vocab.getId().contains("."))
 		{
-			concepts.setSystem("urn:oid:"+vocab.getId());
-			
+			system= "urn:oid:"+vocab.getId();
 			if (vocab.getId().contains("2.16.840.1.113883.2.1.9") || (vocab.getId().contains("2.16.840.1.113883.12.")))
 			{
 				vocabName = "NHSITK-v2-"+vocabName;
@@ -82,10 +67,9 @@ public class VocabularyToFHIRValueSet implements Processor {
 			{
 				if (vocab.getId().equals("2.16.840.1.113883.2.1.3.2.4.15"))
 				{
-					concepts.setSystem("http://snomed.info/sct");
+					system="http://snomed.info/sct";
 				}
 				vocabName = "NHSITK-v3-"+vocabName;
-				
 			}
 			else
 			{
@@ -95,70 +79,172 @@ public class VocabularyToFHIRValueSet implements Processor {
 		}
 		else if (NumberUtils.isNumber(vocab.getId())) 
 		{
-			concepts.setSystem("http://snomed.info/sct");
+			system = "http://snomed.info/sct";
 			vocabName = "NHSITK-SCT-"+vocabName;
 		}
 		else
 		{
 			// May not be robust
-			concepts.setSystem("http://snomed.info/sct");
+			system = "http://snomed.info/sct";
 		}
+		
 		exchange.getIn().setHeader(Exchange.FILE_NAME, vocabName+".XML");
 		
 		String idStr = vocabName.replace(" ","-").toLowerCase();
-		valueSet.setId(idStr.substring(0, idStr.length()));
-		if (vocab.getDescription() !=null)
+		if (idStr.length()>64)
 		{
-			// Could do with a cleaner conversion method - also need to check xsd
-			String description = "";
-			for (int h = 0;h<vocab.getDescription().getP().size();h++)
-			{
-				description = description + vocab.getDescription().getP().get(h).toString()+" ";
-			}
-			valueSet.setDescription(description);
+			idStr = idStr.substring(0, 64);
 		}
 		else
 		{
-			valueSet.setDescription(vocabName);
-		};	
-		valueSet.setVersion(vocab.getVersion());
-		valueSet.setName(vocabName);
+			idStr = idStr.substring(0, idStr.length());
+		}
 		
-		for (int f=0;f<vocab.getConcept().size();f++)
+		if (system.equals("http://snomed.info/sct"))
 		{
-			ConceptReferenceComponent code = new ConceptReferenceComponent();
-			code.setCode(vocab.getConcept().get(f).getCode().toString());
-			for (int g=0;g<vocab.getConcept().get(f).getDisplayName().size();g++)
+			ValueSet valueSet = new ValueSet();
+			ConceptSetComponent concepts = new ConceptSetComponent();
+			concepts.setSystem(system);
+			
+			Extension extension = new Extension();
+			extension.setUrl("http://hl7.org/fhir/StructureDefinition/valueset-oid");
+			extension.setValue(new UriType("urn:oid:"+vocab.getId()));
+			valueSet.addExtension(extension);
+			
+			extension = new Extension();
+			extension.setUrl("http://west.riding.nhs.uk/fhir/StructureDefinition/vocabulary-filename");
+			extension.setValue(new StringType(exchange.getIn().getHeader(Exchange.FILE_NAME).toString()));
+			valueSet.addExtension(extension);
+			
+			exchange.getIn().setHeader("ActiveStatus", vocab.getStatus());
+			switch(vocab.getStatus())
 			{
-				if (vocab.getConcept().get(f).getDisplayName().get(g).getType() != null)
+				case "active" :
+				case "Active" :
+				case "created" :
+					valueSet.setStatus(ConformanceResourceStatus.ACTIVE);
+					break;
+				case "superseded" :
+					valueSet.setStatus(ConformanceResourceStatus.RETIRED);
+					break;
+				default:
+					valueSet.setStatus(ConformanceResourceStatus.NULL);
+			}
+			valueSet.setId(idStr);
+			
+			if (vocab.getDescription() !=null)
+			{
+				// Could do with a cleaner conversion method - also need to check xsd
+				String description = "";
+				for (int h = 0;h<vocab.getDescription().getP().size();h++)
 				{
-					if (vocab.getConcept().get(f).getDisplayName().get(g).getType().equals("PT"))
+					description = description + vocab.getDescription().getP().get(h).toString()+" ";
+				}
+				valueSet.setDescription(description);
+			}
+			else
+			{
+				valueSet.setDescription(vocabName);
+			};	
+			valueSet.setVersion(vocab.getVersion());
+			valueSet.setName(vocabName);
+			
+			for (int f=0;f<vocab.getConcept().size();f++)
+			{
+				ConceptReferenceComponent code = new ConceptReferenceComponent();
+				code.setCode(vocab.getConcept().get(f).getCode().toString());
+				for (int g=0;g<vocab.getConcept().get(f).getDisplayName().size();g++)
+				{
+					if (vocab.getConcept().get(f).getDisplayName().get(g).getType() != null)
+					{
+						if (vocab.getConcept().get(f).getDisplayName().get(g).getType().equals("PT"))
+						{
+							code.setDisplay(vocab.getConcept().get(f).getDisplayName().get(g).getValue());
+						}
+					}
+					else
 					{
 						code.setDisplay(vocab.getConcept().get(f).getDisplayName().get(g).getValue());
+						
 					}
 				}
-				else
-				{
-					code.setDisplay(vocab.getConcept().get(f).getDisplayName().get(g).getValue());
-					
-				}
+				concepts.addConcept(code);
 			}
-			concepts.addConcept(code);
-		}
-		ValueSetComposeComponent comp = new ValueSetComposeComponent();
-	
-		comp.addInclude(concepts);
+			ValueSetComposeComponent comp = new ValueSetComposeComponent();
 		
-		valueSet.setCompose(comp);
-		
-		String Response = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(valueSet);
+			comp.addInclude(concepts);
 			
-		exchange.getIn().setHeader(Exchange.HTTP_PATH,"/ValueSet/"+valueSet.getId());
+			valueSet.setCompose(comp);
+			
+			String Response = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(valueSet);
+			exchange.getIn().setHeader(Exchange.HTTP_PATH,"/ValueSet/"+valueSet.getId());
+			exchange.getIn().setBody(Response);
+		}
+		else
+		{
+			CodeSystem codeSystem = new CodeSystem();
+			
+			//ConceptSetComponent concepts = new ConceptSetComponent();
+			//concepts.setSystem(system);
+			
+			Extension extension = new Extension();
+			extension = new Extension();
+			extension.setUrl("http://west.riding.nhs.uk/fhir/StructureDefinition/vocabulary-filename");
+			extension.setValue(new StringType(exchange.getIn().getHeader(Exchange.FILE_NAME).toString()));
+			codeSystem.addExtension(extension);
+			
+			codeSystem.setId(idStr);
+			
+			Identifier identifier = new Identifier();
+			identifier.setSystem("urn:ietf:rfc:3986").setValue(vocab.getId());
+			codeSystem.setIdentifier(identifier);
+			
+			codeSystem.setUrl("http://west.riding.nhs.uk/fhir/"+idStr);
+			codeSystem.setName(vocab.name);
+			
+			switch(vocab.getStatus())
+			{
+				case "active" :
+				case "Active" :
+				case "created" :
+					codeSystem.setStatus(ConformanceResourceStatus.ACTIVE);
+					break;
+				case "superseded" :
+					codeSystem.setStatus(ConformanceResourceStatus.RETIRED);
+					break;
+				default:
+					codeSystem.setStatus(ConformanceResourceStatus.NULL);
+			}
+			
+			for (int f=0;f<vocab.getConcept().size();f++)
+			{
+				ConceptDefinitionComponent concept = new ConceptDefinitionComponent();
+				
+				concept.setCode(vocab.getConcept().get(f).getCode().toString());
+				for (int g=0;g<vocab.getConcept().get(f).getDisplayName().size();g++)
+				{
+					if (vocab.getConcept().get(f).getDisplayName().get(g).getType() != null)
+					{
+						if (vocab.getConcept().get(f).getDisplayName().get(g).getType().equals("PT"))
+						{
+							concept.setDisplay(vocab.getConcept().get(f).getDisplayName().get(g).getValue());
+						}
+					}
+					else
+					{
+						concept.setDisplay(vocab.getConcept().get(f).getDisplayName().get(g).getValue());
+					}
+				}
+				codeSystem.addConcept(concept);
+			}
+			String Response = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(codeSystem);
+			exchange.getIn().setHeader(Exchange.HTTP_PATH,"/CodeSystem/"+codeSystem.getId());
+			exchange.getIn().setBody(Response);
+			
+		}
 		exchange.getIn().setHeader(Exchange.HTTP_METHOD, "PUT");
 		exchange.getIn().setHeader(Exchange.HTTP_QUERY,"");
 		exchange.getIn().setHeader(Exchange.CONTENT_TYPE,"application/xml+fhir");
-		exchange.getIn().setBody(Response);
-				
 	}
 
 }
